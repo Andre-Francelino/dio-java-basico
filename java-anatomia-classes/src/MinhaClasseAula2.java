ab354d345f51744bf614a48e8b58464abbdf1c85
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-01-07 17:17:04 -0300

    Anatomia das classes Aula 2

---
+++ java-anatomia-classes/src/MinhaClasseAula2.java
@@ -0,0 +1,27 @@
+public class MinhaClasseAula2 {
+    public static void main(String[] args) {
+
+        int ano = 2024;
+        ano = 2023;
+
+        final String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIROS = 27;
+        int ANO_2000 = 2000;
+
+        // Declaração inválida de variáveis
+        /* 
+        int numero&um = 1;  // Os únicos simbolos permitidos são _ e $
+        int 1numero = 1;    // Uma variável não pode começar com númerico
+        int numero um = 1;  // Não pode ter espaço no nome da variável
+        int long = 1;       // long faz parte das palavras reservadas da linguagem
+        */
+
+        // Declaração válida de variáveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroUm = 1;
+        int longo = 1;
+        
+    }
+}
