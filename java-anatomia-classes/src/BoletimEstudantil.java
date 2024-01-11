76ad41af471dad06c2006f893455e2b3ccf08f52
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-01-11 08:50:15 -0300

    Anatomia de classes Aula 4

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+
+        if (mediaFinal < 6) {
+            System.out.println("Aluno Reprovado!");
+        } else if (mediaFinal == 6) {
+            System.out.println("Prova Minerva - Aluno fará recuperação!");
+        } else  {
+            System.out.println("Aluno Aprovado!");
+            System.out.println("Boas Férias!");
+        }
+    }
+}
+
