16c128cb10a55712ab47ef7f1b31dc7977a7d06d
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-01-17 21:19:20 -0300

    Aulas Operadores Unários e de Incrementos

---
+++ tipos-variaveis/src/OperadoresIncrementos.java
@@ -0,0 +1,13 @@
+public class OperadoresIncrementos {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //numero = numero + 1;
+        System.out.println(numero ++);
+        System.out.println(numero);
+
+        //numero = numero - 1;
+        System.out.println(numero --);
+        System.out.println(numero);
+    }
+}
---
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,14 @@
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+
+        //numero = + numero;
+    // o + é um operador de adição, para somar. Para converter para positivo é necessário usar a multiplicação *
+        numero = numero * -1;
+        System.out.println(numero);
+
+    }
+}
