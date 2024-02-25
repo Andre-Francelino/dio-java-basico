0e3b9cc26fcf3cc795a0b01aff7d23912dee3a4e
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-02-25 11:32:32 -0300

    update CaixaEletronico3

--- controle-fluxo/src/CaixaEletronico3.java
+++ controle-fluxo/src/CaixaEletronico3.java
@@ -7,7 +7,7 @@ public class CaixaEletronico3 {
     */ 
 
     public static void main(String[] args) {
-        double saldo = 25.0;
+        double saldo = 30.0;
         double valorSolicitado = 25.5;
 
         if(valorSolicitado < saldo) {
