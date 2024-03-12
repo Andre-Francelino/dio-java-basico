8dc41cb267188aad1d253f934ac72b635ee52ae1
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-03-11 23:15:12 -0300

    Aula2 - Estruturas de Repetição - FOR

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String atores[] = {"RILEY", "LANA", "MIKE", "LOLLAH", "ROGER", "MÔNICA", "EVA", "DREDD", "JAYSLIN", "LOGAN", "VIVIAN", "JEFF", "KAREN", "YASMIN"};
+
+        for(int x = 0; x < atores.length; x ++) {
+            System.out.println("O ator no indice x = " + x + " é " + atores[x]);
+        }
+    }
+}
---
+++ controle-fluxo/src/ExemploForEach.java
@@ -0,0 +1,10 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String animes [] = { "Dragon Ball Z", "Yu Yu Hakusho", "One Piece", "Os Cavaleiros do Zodiaco", "Naruto", "Samurai X", "Pokemon", "Bayblade", "Akira", "Cowboy Bebop"};
+
+        for(String anime : animes) {
+            System.out.println("O nome do anime é: " + anime);
+        }
+    }
+    
+}
