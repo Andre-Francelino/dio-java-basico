bb60601bf286da6de3f4aca426d03f18ccfeb0b8
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-03-31 21:38:36 -0300

    Resolvendo desafios

---
+++ controle-fluxo/src/DesafioEntradaSaidaNomes.java
@@ -0,0 +1,29 @@
+/*
+    Faça um programa com as características abaixo:
+
+        1. Leia 10 nomes, sem espaço em branco;
+        2. Imprima o terceiro nome da lista;
+        3. Imprima o sétimo nome da lista;
+        4. Imprima o nono nome da lista. 
+
+ */
+
+import java.util.Scanner;
+
+public class DesafioEntradaSaidaNomes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] nomes = new String[10];
+
+        for(int i = 0; i < 10; i++) {
+            nomes[i] = sc.nextLine();
+        }
+
+        System.out.println(nomes[2]);
+        System.out.println(nomes[6]);
+        System.out.println(nomes[8]);
+
+        sc.close();
+    }
+}
